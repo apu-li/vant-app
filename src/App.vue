035815ee416376e1c58035a966909eeea3c4c0bc
Pu-feng <2542648484@qq.com>
2022-05-22 15:01:27 +0800

    安装重置样式reset-css

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ body {
 // 移动端单位 rem em相对当前节点的字体大小
 html {
   // 1rem就是100px
-  font-size: 100px;
+  font-size: 100px !important;
 }
 // 问题：如果此时要对字体设置16px,应该写成多少rem?   0.16
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <van-search v-model="searchVal" shape="round" background="#fff" placeholder="请输入搜索关键词" disabled />
+    <p>1111</p>
   </div>
 </template>
 
